6d5b1ba1772c2300f2319f84f7f295bd79dd36dd
Richard Pentecost <richardpentecost1@gmail.com>
2019-02-07 21:18:01 +0000

    Booleans completed

--- __tests__/booleans.test.js
+++ __tests__/booleans.test.js
@@ -101,7 +101,7 @@ describe('isLessThanOrEqualTo', () => {
 });
 
 describe('isOdd', () => {
-  xit('returns whether the number is odd', () => {
+  it('returns whether the number is odd', () => {
     expect(isOdd(5)).toBe(true);
     expect(isOdd(6)).toBe(false);
     expect(isOdd(7)).toBe(true);
@@ -110,7 +110,7 @@ describe('isOdd', () => {
 });
 
 describe('isEven', () => {
-  xit('returns whether the number is even', () => {
+  it('returns whether the number is even', () => {
     expect(isEven(5)).toBe(false);
     expect(isEven(6)).toBe(true);
     expect(isEven(7)).toBe(false);
@@ -119,7 +119,7 @@ describe('isEven', () => {
 });
 
 describe('isSquare', () => {
-  xit('returns true if the number is a square', () => {
+  it('returns true if the number is a square', () => {
     expect(isSquare(9)).toEqual(true);
     expect(isSquare(5)).toEqual(false);
     expect(isSquare(-4)).toEqual(false);
@@ -128,7 +128,7 @@ describe('isSquare', () => {
 });
 
 describe('startsWith', () => {
-  xit('returns whether the given string starts with the given character', () => {
+  it('returns whether the given string starts with the given character', () => {
     expect(startsWith('a', 'aardvark')).toBe(true);
     expect(startsWith('a', 'qaardvark')).toBe(false);
     expect(startsWith('a', 'Aardvark')).toBe(false);
@@ -136,7 +136,7 @@ describe('startsWith', () => {
 });
 
 describe('containsVowels', () => {
-  xit('returns whether the given string contains vowels', () => {
+  it('returns whether the given string contains vowels', () => {
     expect(containsVowels('cat')).toBe(true);
     expect(containsVowels('DOG')).toBe(true);
     expect(containsVowels('why')).toBe(false);
@@ -144,7 +144,7 @@ describe('containsVowels', () => {
 });
 
 describe('isLowerCase', () => {
-  xit('it returns true if the given string is lowercase', () => {
+  it('it returns true if the given string is lowercase', () => {
     expect(isLowerCase('abc')).toBe(true);
     expect(isLowerCase('abc213')).toBe(true);
     expect(isLowerCase('Abc')).toBe(false);
--- src/booleans.js
+++ src/booleans.js
@@ -27,30 +27,48 @@ const isGreaterThan = (a, b) => a > b;
 
 const isLessThanOrEqualTo = (a, b) => a <= b;
 
-const isOdd = (a) => {
-  // your code here
-};
+const isOdd = (a) => a % 2 === 1;
 
-const isEven = (a) => {
-  // your code here
-};
+const isEven = (a) => a % 2 === 0;
 
 const isSquare = (a) => {
-  // your code here
+  let total;
+  let iterator = 0;
+  do {
+    total = iterator ** 2;
+    iterator++;
+    if (total === a) {
+      return true;
+    }
+  } while (total < a) 
+  return false;
 };
 
-const startsWith = (char, string) => {
-  // your code here
-};
+const startsWith = (char, string) => string[0] === char;
 
 const containsVowels = (string) => {
-  // your code here
+  const vowels = ['a', 'e', 'i', 'o', 'u']
+  for (let i = 0; i < string.length; i++) {
+    for (let n = 0; n < vowels.length; n++) {
+      if (string[i].toLowerCase() === vowels[n]) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 const isLowerCase = (string) => {
-  // your code here
+  for (let i = 0; i < string.length; i++) {
+    //console.log(string.charCodeAt(1);
+    if (string.charCodeAt(i) > 64 && string.charCodeAt(i) < 91) {
+      return false;
+    }
+  }
+  return true;
 };
 
+//isLowerCase('Abc')
 module.exports = {
   negate,
   both,
